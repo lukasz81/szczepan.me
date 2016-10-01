9fa50578d14b79ea6322a000d4a6744dfda28f74
lukasz81 <info@designmasters.co.uk>
2016-10-01 15:55:01 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	css/responsive.css
    #	css/responsive.css.map
    #	css/responsive.scss
    #	css/styles.css
    #	css/styles.css.map
    #	css/styles.scss
    #	index.html
    #	js/scripts.js
    #	svg/svg_logo_simple.svg

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,28 @@
 //copyright: Lukasz Szczepanski
+//change role text-align
+var changRoleText = function(){
+	roles = ['UI Designer','UI Developer','Front End Developer','Designer','UI Engineer','Web Designer','Web Developer','Coder','Human'];
+	var role = $('.role')
+	setInterval(function(){
+		var item = roles[Math.floor(Math.random()*roles.length)];
+		var txt  = item + '.'
+		role.text(txt).attr('data-text',txt);
+	},5000);
 
+}
 //random bg gradient color
 var changeBgColor = function(userTriggered){
 	a = 1
-	stop1 = 'rgba(' + createColor() + ',' + createColor() + ',' + createColor() + ',' + a + ')'
-	stop2 = 'rgba(' + createColor() + ',' + createColor() + ',' + createColor() + ',' + a + ')'
-	stop1 = stop1.toString()
-	stop2 = stop2.toString()
-	gradient    = 'linear-gradient(45deg,' + stop1 + ',' + stop2 + ')'
-	revGradient = 'linear-gradient(-45deg,' + stop2 + ',' + stop1 + ')'
+	stop1 = 'rgba(' + createColor() + ',' + createColor() + ',' + createColor() + ',' + a + ')';
+	stop2 = 'rgba(' + createColor() + ',' + createColor() + ',' + createColor() + ',' + a + ')';
+	stop3 = 'rgba(' + createColor() + ',' + createColor() + ',' + createColor() + ',' + 0 + ')'
+	stop1 = stop1.toString();
+	stop2 = stop2.toString();
+	gradient    = 'linear-gradient(45deg,' + stop1 + ',' + stop2 + ')';
+	halfGrad    = 'linear-gradient(0deg,' + stop1 + ',' + stop3 + ')';
+	revGradient = 'linear-gradient(-45deg,' + stop2 + ',' + stop1 + ')';
 	window.stop1 = stop1;
-	window.stop2 = stop2
+	window.stop2 = stop2;
 	$('.outer').css({
 		'background': gradient
 	}).addClass('active');
@@ -23,26 +35,34 @@ var changeBgColor = function(userTriggered){
 	$('.inner').addClass('active');
 	$('#style').remove();
 	$('<style id="style">' +
-		'.code .codeDetails ul li .imagesContainer .imgWrapp::after , .code .codeDetails ul li .imagesContainer .imgWrapp::before {background:' + stop2 + ';} ' +
-		'.code .stop1-text{color:' + stop2 + ';} ' +
-		'.design .designDetails ul li .imagesContainer .imgWrapp::before,.design .designDetails ul li .imagesContainer .imgWrapp::after{background:' + stop1 + ';} ' +
+		'.stop2 .imgWrapp::after , .stop2 .imgWrapp::before {background:' + stop2 + ';} ' +
+		'.stop1-text{color:' + stop2 + ';} ' +
+		'.stop1 .imgWrapp::before , .stop1 .imgWrapp::after {background:' + stop1 + ';} ' +
+		'.instaImagesBg::after {background:' + halfGrad + '} ' +
+		'.instaImagesBg {background:' + stop2 + '} ' +
+		'.comingSoon .glitch:before{text-shadow:2px 0 ' + stop2 + '} ' +
+		'.comingSoon .glitch:after{text-shadow:2px 0 ' + stop1 + '} ' +
 	  '</style>').appendTo('head');
 }
 //find number between 0 to 255
-function createColor(){
+var createColor = function(){
 	return Math.floor(Math.random() * (255 - 0 + 1)) + 0
 }
 //detect touch device
-function isTouch(){
+var isTouch = function(){
 	var touch = $('html').hasClass('touchevents') ? true : false;
 	return touch;
 }
 // detect iOS
 var iOS = !!navigator.platform && /iPad|iPhone|iPod/.test(navigator.platform);
 // window height & width
-var width = $(window).outerWidth();
-var height;
-isTouch() ? height = window.screen.height : height = $(window).outerHeight();
+var getScreenSizes = function(){
+	var width  = isTouch() ? window.screen.width : $(window).outerWidth();
+	var height = isTouch() ? window.screen.height : $(window).outerHeight();
+	return {width,height};
+}
+var width  = getScreenSizes().width;
+var height = getScreenSizes().height;
 //get current period of a day and change text in hero
 function getCurrentTime() {
 	var today = new Date()
@@ -70,14 +90,15 @@ function instAPI(){
 }
 var callbackFunction = function(json){
 	var o = json.data;
+	//console.log(json);
 	for ( i=0; i < o.length; i++ ){
 		var imageURL = o[i].images.standard_resolution.url;
-		var image    = '<a href="#"><img src="' + imageURL + '"/></a>';
-		$(image).appendTo('#instafeed');
+		var image    = '<a class="instaLink" href="#"><img src="' + imageURL + '"/></a>';
+		$(image).appendTo('#instafeed .instaImagesBg');
 	}
 };
 //load infor from xml and create some html
-function getDataFromXml(){
+var getDataFromXml = function(){
 	path = window.location.href + 'images/dev-icons/';
 	$.get(path + 'data.xml', function(d){
 		var skill  = $(d).find('skill');
@@ -108,15 +129,90 @@ function getDataFromXml(){
 		addClickEvents('.imgWrapp');
 	});
 }
+//click events for click elements
+window.active = false;
+var addClickEvents = function(elm){
+	$(elm).off('click').click(function(){
+		var elm       = $(this);
+		var target    = elm.parents().hasClass('code') ? 'code' : 'design';
+		var element   = $('.' + target);
+		var container = $(element).find('.detailsContainer li').first();
+		var method    = elm.hasClass('closeUI') ? 'close' : 'open';
+		window.active = true;
+		if (target == 'code'){
+		  var index     = elm.index();
+		  var height    = container.outerHeight();
+		  if (elm.hasClass('tinyButton') || elm.hasClass('closeUI')){
+		  	var margin = (index)*height;
+		  	$(this).addClass('active').siblings().removeClass('active');
+		  	addAnimationsToSkills(index-1,method);
+		  }else{
+		  	var margin = (index+1)*height;
+		  	$('.tinyButton').eq(index+1).addClass('active').siblings().removeClass('active');
+		  	addAnimationsToSkills(index,method);
+		  };
+	  }else{
+			elm.addClass('isOn')
+			//elm.siblings('.imgWrapp').removeClass('isOn').addClass('isOff');
+			var imageURL  = window.location.href + 'images/FG-LP.jpg';
+			var className = 'image-content-wrapp'
+			var divTag    = '<div class=' + className + '><img class="image-content" src="' + imageURL + '" /></div>';
+			var e         = $('.' + className);
+			if (!e.length){
+				$('.isOn').after(divTag);
+			}else if(method == 'close'){
+				$('.' + className).removeClass('show');
+			}
+			$('.detailsContainer.images-li').addClass('scrollable')
+			animateDesignButtons(elm);
+		};
+		reactAndScrollTop(element,container,margin,method);
+	});
+};
+//animate design buttons
+var animateDesignButtons = function(elm){
+	//var className = elm.className;
+	var siblings  = elm.siblings('.imgWrapp');
+	var delay     = 200;
+	for (var i = 0; i < siblings.length; i++){
+		var sib   = siblings;
+		var index = 0;
+		setTimeout(function(){
+			timedEvenet(sib[index]);
+			index++;
+		},delay*i)
+	}
+};
+var timedEvenet = function(sib){
+	$(sib).addClass('isOff');
+	$('.image-content-wrapp').addClass('show');
+}
+//adjust scrollTop for the elements
+var reactAndScrollTop = function(element,container,margin,method){
+	var domPos    = getScreenSizes().width <= 768 ? element.index()*2 : element.index();
+	var distance  = domPos*height;
+	var delay     = element.hasClass('activated') ? '0' : '500';
+	//react after scrolling the container to top
+	$('body,html').animate({scrollTop:distance}, delay, function() {
+   	method == 'open' ? element.addClass('activated') : element.removeClass('activated');
+		if (method == 'close'){
+			$('.isOff,.isOn').removeClass('isOff isOn');
+		}
+		//window.active = false;
+		container.css({
+			'margin-top':-margin,
+		})
+	});
+};
 //create little dot navigation
-function createDotsNav(){
+var createDotsNav = function(){
 	var list      = $('.codeDetails .detailsContainer li')
 	var length    = list.length;
 	var container = $('.codeDetails');
 	container.append('<nav class="tinyNav"></nav>')
 	list.each(function(index,el){
 		nameData = $(this).find('h3').text();
-		(nameData.length == 0)? nameData = 'All' : nameData
+		nameData.length == 0 ? nameData = 'All' : nameData;
 		$('.tinyNav').append('<span data-for="' + nameData + '" class="tinyButton"></span>');
 	});
 	$('.tinyButton').eq(0).addClass('active')
@@ -125,32 +221,7 @@ function createDotsNav(){
 		'background': stop2
 	});
 };
-//click events for click elements
-window.active = false;
-function addClickEvents(elm){
-	$(elm).off('click').click(function(){
-		window.active = true;
-		var elm       = $(this);
-		var index     = elm.index();
-		var container = $('.detailsContainer li').first();
-		var height    = container.outerHeight();
-		if (elm.hasClass('tinyButton') || elm.hasClass('closeUI')){
-			var margin = (index)*height;
-			$(this).addClass('active').siblings().removeClass('active');
-			addAnimationsToSkills(index-1,method);
-		}else{
-			var margin = (index+1)*height;
-			$('.tinyButton').eq(index+1).addClass('active').siblings().removeClass('active');
-			addAnimationsToSkills(index,method);
-		};
-		var method;
-		elm.hasClass('closeUI')? method='close' : method='open';
-		var elm = $('.code');
-		//removeClassForEach('remove',elm);
-		reactToScrollTop(elm,container,margin,method);
-	});
-};
-function addAnimationsToSkills(index,method){
+var addAnimationsToSkills = function(index,method){
 	var max     = 15;
 	var li      = $('.codeDetails li');
 	var seq     = index+1;
@@ -158,8 +229,8 @@ function addAnimationsToSkills(index,method){
 	var skill   = $(elm).find('p.skill');
 	var _length = $(elm).find('p.length');
 	var s,l;
-	skill.data('knowledge')? s=skill.data('knowledge') : s=0;
-	_length.data('length')? l=_length.data('length') : l=0;
+	skill.data('knowledge') ? s=skill.data('knowledge') : s = 0;
+	_length.data('length') ? l=_length.data('length') : l = 0;
 	var sBar    = skill.find('span.bar');
 	var sVal    = skill.find('span.val');
 	var lBar    = _length.find('span.bar');
@@ -175,32 +246,19 @@ function addAnimationsToSkills(index,method){
 		}
 	});
 }
-//adjust scrollTop for the elements
-function reactToScrollTop(elm,container,margin,method){
-	var delay = '500';
-	domPos    = elm.index();
-	distance  = domPos*height;
-	elm.hasClass('activated')? delay='0' : delay='500';
-	$('body,html').animate({scrollTop:distance}, delay, function() {
-   	method=='open'? elm.addClass('activated') : elm.removeClass('activated');
-		window.active = false;
-		container.css({
-			'margin-top':-margin,
-		})
-	});
-};
-//isOdd?
-function isOdd(num){return num % 2};
+//is Odd?
+var isOdd = function(num){return num % 2};
 //mouse position
-$(document).on( "mousemove", function( event ) {
-	if ($('.code').hasClass('activated')){
+$(document).on('mousemove', function( event ) {
+	var activated = $('.code').hasClass('activated');
+	if (activated){
 	  var x = event.pageX*0.1;
 	  var y = event.pageY*0.1;
 	  var panText = $('.stop1-text');
 	  for (i=0; i<panText.length; i++){
 	  	var index = panText[i];
 	  	var dest;
-	  	isOdd(i)? dest='-':dest="+";
+	  	isOdd(i) ? dest = '-' : dest = '+';
 	  	var valX = dest+y;
 	  	var valY = dest+x;
 	    $(panText).eq(i).css({
@@ -215,7 +273,7 @@ $(document).on( "mousemove", function( event ) {
 		return
 	}
 });
-$(document).on('scroll',function(){
+$(document).on('scroll', function(){
 		var codeArticle    = $('.code article')[0];
 		var codeDetails    = $('.code .codeDetails')[0];
 		var designArticle  = $('.design article')[0];
@@ -230,24 +288,27 @@ $(document).on('scroll',function(){
 		checkDistanceFromTop([codeArticle,codeDetails,designArticle,designDetails,aboutMe]);
 		$('.article-h2 span').css('background-position', mommentum+'px 0');
 		$('.code article').css('background-position', bgMommentum + "% 50%");
-		$('.design article').css('background-position', bgMommentum/5 + "% 50%");
+		$('.design article').css('background-position', 100-bgMommentum + "% 50%");
+		//$('.design .overlay,.code .overlay').css('background-position', docTop*0.1+ "% 50%");
 });
-function checkDistanceFromTop(el){
-	for (i=0; i<el.length; i++){
+var checkDistanceFromTop = function(el){
+	for (var i=0; i<el.length; i++){
 		var e     = el[i];
 		var top   = e.getBoundingClientRect().top;
 		var half  = height/1.5;
 		if (top <= half && top > half/8 && $(e).hasClass('def')){
 			$(e).removeClass('def');
 			removeClassForEach('remove',e);
+			console.log('a');
 		}else if ($(document).scrollTop() < 100){
 			$(e).addClass('def');
+			console.log('b');
 			if (!window.active){
 			  removeClassForEach('add',e);
+				console.log('c');
 		  }
 		}else if (top <= height/1.2){
 			if ($(e).hasClass('designDetails')){
-				//alert('d');
 				$(e).find('.imagesContainer').css({
 					'-webkit-transform' : 'translateY(' + -top*0.15 + 'px)',
 					'-moz-transform'    : 'translateY(' + -top*0.15 + 'px)',
@@ -260,7 +321,7 @@ function checkDistanceFromTop(el){
 	};
 }
 //remove class for each element and animate
-function removeClassForEach(type,e){
+var removeClassForEach = function(type,e){
 	var imgWrapp = $(e).find('.imgWrapp');
 	if (imgWrapp.length > 0){
 		for (i=0;i<imgWrapp.length;i++){
@@ -268,7 +329,7 @@ function removeClassForEach(type,e){
 		}
 	}
 }
-function delayedReact(index,type,el){
+var delayedReact = function(index,type,el){
 	setTimeout(function(){
 		if (type == 'remove'){
 			el.eq(index).removeClass('def')
@@ -279,16 +340,31 @@ function delayedReact(index,type,el){
 }
 //resize update
 $(window).resize(function(){
-	layoutsAdjust();
+	if (width != $(window).outerWidth()){
+		layoutsAdjust();
+		console.log('widthO: ' + width, 'widthN: '+$(window).outerWidth());
+	}
 });
+//open navigation and events
+var openNav = function(){
+	$('.hamburger').click(function(){
+		$(this).toggleClass('is-active');
+	})
+}
 //after document has loaded
 $(document).ready(function(){
 	layoutsAdjust();
-	getCurrentTime();
+	// if (width > 768){
+	// 	getCurrentTime();
+	// };
 	getDataFromXml();
 	instAPI();
-	//load big loooong xml svg file, don't inline!
-	loadSVGs();
+	changRoleText();
+	openNav();
+	$('.images-show').click(function(e){
+		e.preventDefault();
+		alert('#TODO');
+	});
 	//little hero heart click events
 	$('.heart').click(function(){
 		$('.inner , .outer').removeClass('active');
@@ -297,26 +373,24 @@ $(document).ready(function(){
 		}, 300);
 	})
 });
-function layoutsAdjust(){
-	(height < 700 && !isTouch())? height = 700 : height;
-	var min;
-	(width < 768) ? min=height*2 : min=height;
+var layoutsAdjust = function(){
+	height < 700 && !isTouch() ? height = 700 : height;
+	var min = (getScreenSizes().width <= 768) ? height*2 : height;
 	console.log(height,min,width);
 	$('.code , .design').css({
-		'height':height,
+		'height':min,
 		'min-height':min,
 	});
 	$('.outer , .full-height').css({
 		'height':height,
-	})
-	if (iOS && window.screen.height==568){
+	});
+	if (iOS && window.screen.height == 568){
 		$(window).scrollTop(85);
 	};
 }
-function loadSVGs(){
-	$('.inner').load('svg/svg_logo.xml');
-	$('.behance').load('svg/behance.svg');
-	$('.github').load('svg/gitHub.svg');
-	$('.code-shield').load('svg/code-shield.svg')
-	$('.design-shield').load('svg/design-shield.svg')
-}
+var callback = function(response, status, xhr){
+	if ( status == 'error' ) {
+	    var msg = 'Sorry but there was an error: ';
+	    console.log( msg + xhr.status + " " + xhr.statusText );
+	  }
+};
