63a62f446b55b5786766d965d123c00b416547a0
lukasz81 <info@designmasters.co.uk>
2018-07-23 12:11:54 +0100

    Change node version in config

--- dist/.circleci/deploy.js
+++ dist/.circleci/deploy.js
@@ -1,16 +1,16 @@
-"use strict";
+'use strict';
 
 var FtpDeploy = require('ftp-deploy');
 var ftpDeploy = new FtpDeploy();
 
 var config = {
-    username: process.env.FTPUSERNAME,
-    password: process.env.FTPPASS,
-    host: process.env.FTPHOST,
-    port: 21,
-    localRoot: __dirname + "/../dist/",
-    remoteRoot: "/",
-    include: ['*']
+    // username: process.env.FTPUSERNAME,
+    // password: process.env.FTPPASS,
+    // host: process.env.FTPHOST,
+    // port: 21,
+    // localRoot: __dirname + "/../dist/",
+    // remoteRoot: "/",
+    // include: ['*']
 };
 
 ftpDeploy.deploy(config, function (err) {
