9487222a930f3fc60c92effb3892e0be2161fef1
lukasz81 <info@designmasters.co.uk>
2018-07-23 15:29:26 +0100

    update ftp-deploy config

--- .circleci/deploy.js
+++ .circleci/deploy.js
@@ -2,13 +2,14 @@ let FtpDeploy = require('ftp-deploy');
 let ftpDeploy = new FtpDeploy();
 
 let config = {
-    username: process.env.FTPUSERNAME,
+    user: process.env.FTPUSERNAME,
     password: process.env.FTPPASS,
     host: process.env.FTPHOST,
     port: 21,
     localRoot: __dirname + "/../dist/",
     remoteRoot: "/extras/ci-test",
-    include: ['*']
+    include: ['*'],
+    deleteRemote: true
 };
 
 ftpDeploy.deploy(config, function(err) {
--- dist/.circleci/deploy.js
+++ dist/.circleci/deploy.js
@@ -4,13 +4,14 @@ var FtpDeploy = require('ftp-deploy');
 var ftpDeploy = new FtpDeploy();
 
 var config = {
-    username: process.env.FTPUSERNAME,
+    user: process.env.FTPUSERNAME,
     password: process.env.FTPPASS,
     host: process.env.FTPHOST,
     port: 21,
     localRoot: __dirname + "/../dist/",
     remoteRoot: "/extras/ci-test",
-    include: ['*']
+    include: ['*'],
+    deleteRemote: true
 };
 
 ftpDeploy.deploy(config, function (err) {
