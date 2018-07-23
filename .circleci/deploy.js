593b03d7ae6a7a7899deb3ef145018a96b6f9200
lukasz81 <info@designmasters.co.uk>
2018-07-23 10:59:43 +0100

    Add deployment script

---
+++ .circleci/deploy.js
@@ -0,0 +1,17 @@
+let FtpDeploy = require('ftp-deploy');
+let ftpDeploy = new FtpDeploy();
+
+let config = {
+    username: process.env.FTPUSERNAME,
+    password: process.env.FTPPASS,
+    host: process.env.FTPHOST,
+    port: 21,
+    localRoot: __dirname + "/../dist/",
+    remoteRoot: "/",
+    include: ['*']
+};
+
+ftpDeploy.deploy(config, function(err) {
+    if (err) console.log(err)
+    else console.log('finished');
+});
