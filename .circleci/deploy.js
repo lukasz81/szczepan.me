5d487c2dfc20878503844075217bad5596ae0377
lukasz81 <info@designmasters.co.uk>
2018-07-24 11:51:56 +0100

    Try out include just css and dist

--- .circleci/deploy.js
+++ .circleci/deploy.js
@@ -13,8 +13,8 @@ let config = {
     port: 21,
     localRoot: __dirname + "/../",
     remoteRoot: "/extras/ci-test",
-    include: ['css'],
-    exclude: [],
+    include: ['/css','/dist'],
+    exclude: ['*.'],
     deleteRemote: false
 };
 
